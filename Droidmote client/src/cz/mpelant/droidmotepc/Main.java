ad10beb41272efcecfc08b77f8d736889bffde0a
martinpelant <martinpelant@gmail.com>
2011-01-29 19:47:22 +0000

    PC client update - code clean up, LOGs moved to sysout

--- Droidmote client/src/cz/mpelant/droidmotepc/Main.java
+++ Droidmote client/src/cz/mpelant/droidmotepc/Main.java
@@ -4,7 +4,7 @@ package cz.mpelant.droidmotepc;
 public class Main {
 	public static void main(String s[]) {
 	    MainWindow frame = new MainWindow();
-
+	    frame.pack();
 	    frame.setVisible(true);
 	}
 }
